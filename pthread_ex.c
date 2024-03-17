8decb563f3cb181d646b9533db9fa8d9ced76b34
jel221 <jlaurenceleigh@gmail.com>
2024-03-16 22:08:37 -0700

    Project 2 Demo

--- pthread_ex.c
+++ pthread_ex.c
@@ -4,13 +4,17 @@
 #include <unistd.h>
 
 pthread_t main_tid;
+pthread_t child_tid;
+
 void* helper2(void* a) {
-  sleep(3);
-  printf("This one may not get to run in Project 2.\n");
+  pthread_join(child_tid, NULL);
+  sleep(1);
+  printf("This one will not get to run in Project 2.\n");
   return NULL;
 }
 
 void* helper(void* a) {
+  child_tid = pthread_self();
   pthread_join(main_tid, NULL);
   pthread_t t;
   pthread_create(&t, NULL, &helper2, NULL);
